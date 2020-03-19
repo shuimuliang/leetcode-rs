2348d77ecdd08e3a60837daf16f514e7d028048f
shuimuliang@gmail.com <shuimuliang@gmail.com>
2020-03-20 00:11:53 +0800

    longest-increasing-subsequence

---
+++ src/longest_increasing_subsequence.rs
@@ -0,0 +1,49 @@
+// https://leetcode-cn.com/problems/longest-increasing-subsequence/
+
+pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+    let mut b_array: Vec<i32> = Vec::with_capacity(nums.len());
+
+    for value in &nums {
+        let pos_res = b_array.binary_search(value);
+
+        match pos_res {
+            // 匹配到, 重复元素不做任何处理
+            Ok(_) => {}
+
+            // 未匹配到，根据位置判定
+            Err(pos) => {
+                if (pos as i32) == 0 {
+                    // 空数组，放第一个
+                    if b_array.is_empty() {
+                        b_array.push(*value);
+                    } else {
+                        // 非空数组，替换第一个
+                        if *value < b_array[0] {
+                            b_array[0] = *value;
+                        }
+                    }
+                } else if pos < b_array.len() {
+                    b_array[pos] = *value;
+                } else {
+                    b_array.push(*value);
+                }
+            }
+        }
+        println!("after {:?}", b_array);
+    }
+    b_array.len() as i32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::length_of_lis;
+
+    #[test]
+    fn case_1() {
+        let input = vec![10,9,2,5,3,7,101,18];
+        let output= length_of_lis(input);
+        assert_eq!(4, output);
+    }
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod min_stack_lcci;
 mod reverse_linked_list;
 mod compress_string_lcci;
 mod best_time_to_buy_and_sell_stock;
+mod longest_increasing_subsequence;
 
 fn main() {
 
