98ae3449dec6443ab74056d48ab32b47086e6f42
shuimuliang@gmail.com <shuimuliang@gmail.com>
2020-03-19 23:56:57 +0800

    reverse_linked_list

--- src/reverse_linked_list.rs
+++ src/reverse_linked_list.rs
@@ -36,6 +36,31 @@ mod tests {
 
     #[test]
     fn case_1() {
+        // 1->2->3->4->5->NULL
+        let l5 = Box::new(ListNode::new(5));
+        let mut l4 = Box::new(ListNode::new(4));
+        let mut l3 = Box::new(ListNode::new(3));
+        let mut l2 = Box::new(ListNode::new(2));
+        let mut l1 = Box::new(ListNode::new(1));
+        l4.next = Some(l5);
+        l3.next = Some(l4);
+        l2.next = Some(l3);
+        l1.next = Some(l2);
+
+        let head= reverse_list(Some(l1));
+        let rev_l1 = head.unwrap();
+        assert_eq!(5, rev_l1.val);
+        let rev_l2 = rev_l1.next.unwrap();
+        assert_eq!(4, rev_l2.val);
+        let rev_l3 = rev_l2.next.unwrap();
+        assert_eq!(3, rev_l3.val);
+        let rev_l4 = rev_l3.next.unwrap();
+        assert_eq!(2, rev_l4.val);
+        let rev_l5 = rev_l4.next.unwrap();
+        assert_eq!(1, rev_l5.val);
+        println!("{:?}", rev_l5.next);
+        let tail = rev_l5.next;
+        assert!(tail.is_none());
     }
 
 }
