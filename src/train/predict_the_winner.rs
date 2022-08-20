cdf585bad06987406133cb6d264a5d71bf3e89bf
shuimuliang <shuimuliang@gmail.com>
2022-08-20 20:39:36 +0800

    Update predict_the_winner.rs

--- src/train/predict_the_winner.rs
+++ src/train/predict_the_winner.rs
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/predict-the-winner/solution/
 
-use std::cmp::{max, min};
+use std::cmp::max;
 
+/*
 #[derive(Debug)]
 enum Direction {
     Negative = -1,
@@ -16,43 +17,39 @@ impl Direction {
         }
     }
     fn val(&self) -> i32 {
-        // impl into
         match self {
             Self::Negative => Self::Negative as i32,
             Self::Positive => Self::Positive as i32,
         }
     }
 }
+*/
 
-fn scores(nums: &[i32], start: usize, end: usize, direction: Direction) -> i32 {
-    let score = {
-        if start == end {
-            direction.val() * nums[start]
-        } else {
-            match direction {
-                Direction::Positive => {
-                    let a = nums[start] + scores(nums, start + 1, end, direction.reverse());
-                    let b = nums[end] + scores(nums, start, end - 1, direction.reverse());
-                    // A try to add max to maximize self scores
-                    max(a, b)
-                }
-                Direction::Negative => {
-                    let a = -nums[start] + scores(nums, start + 1, end, direction.reverse());
-                    let b = -nums[end] + scores(nums, start, end - 1, direction.reverse());
-                    // B try to add min to reduce a's scores
-                    min(a, b)
-                }
-            }
-        }
-    };
-    score
+fn scores(nums: &[i32], start: usize, end: usize, memo: &mut Vec<Vec<i32>>) -> i32 {
+    if start == end {
+        // 此时只有一种选择，选的人赢对方nums[i]，且没有剩余可选，结束递归
+        return nums[start];
+    }
+    if memo[start][end] != i32::MIN {
+        return memo[start][end];
+    }
+    // 选择左端，获得nums[i]，之后输掉helper(i+1,j)分
+    let a = nums[start] - scores(nums, start + 1, end, memo);
+    // 选择右端，获得nums[j]，之后输掉helper(i,j-1)分
+    let b = nums[end] - scores(nums, start, end - 1, memo);
+    // 返回较大者，即在[i,j]数组游戏中胜过对方的分数
+    let max_score = max(a, b);
+    memo[start][end] = max_score;
+    max_score
 }
 
 struct Solution;
 
 impl Solution {
     pub fn predict_the_winner(nums: Vec<i32>) -> bool {
-        scores(&nums, 0, nums.len() - 1, Direction::Positive) >= 0
+        let matrix_len = nums.len();
+        let mut memo: Vec<Vec<i32>> = vec![vec![i32::MIN; matrix_len]; matrix_len];
+        scores(&nums, 0, nums.len() - 1, &mut memo) >= 0
     }
 }
 
